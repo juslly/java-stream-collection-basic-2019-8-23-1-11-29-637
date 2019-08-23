7bd5da6e305b49e1a478ca9f1d3436ff3ead90de
zhangjing <juslly@163.com>
2019-08-23 16:48:51 +0800

    push second test

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -36,7 +36,27 @@ public class MyMap {
         //throw new NotImplementedException();
     }
 
-//    public List<Integer> sortFromSmall() {
-//        throw new NotImplementedException();
-//    }
+    public List<Integer> sortFromSmall() {
+    	
+    	for (int i = 0;i<array.size()-1;i++) {
+
+    		for (int j = 0; j < array.size()-i-1;j++) {
+    			if(array.get(j)>array.get(j+1)) {
+    				int temp;
+    				temp = array.get(j);
+    				array.set(j, array.get(j+1));
+    				array.set(j+1, temp);
+
+    				
+    			}
+    			
+    		}
+    		
+    	}
+    	
+		return array;
+    	
+    	
+       // throw new NotImplementedException();
+    }
 }
